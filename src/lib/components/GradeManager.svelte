5c4d3335eeb8081bb63f4c7aa687e37ff69a50f3
Kyle Feng <kylefeng28@gmail.com>
2026-04-08 20:45:36 -0700

    fix: allow entering decimal grades

--- src/lib/components/GradeManager.svelte
+++ src/lib/components/GradeManager.svelte
@@ -84,6 +84,7 @@
 					bind:value={newScore}
 					min="0"
 					max="100"
+					step="any"
 					class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
 				/>
 			</div>
@@ -133,6 +134,7 @@
 											bind:value={editScore}
 											min="0"
 											max="100"
+											step="any"
 											class="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
 										/>
 									</td>
